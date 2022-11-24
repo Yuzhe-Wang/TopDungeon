6b0beddd14d2bdabd4c63d57d7d4dcb065dcc519
Ryan (Yuzhe) Wang <yuzhew@usc.edu>
2022-11-23 18:47:12 -0800

    Finished game save and load, and the portal

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager manager;
+
+    private void Awake()
+    {
+        manager = this;
+    }
+
+    // Resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    // References
+    public Player player;
+
+    // Logic
+    public int pesos;
+    public int experience;
+
+    // Save/Load State
+    /*
+     * INT preferedSkin
+     * INT pesos
+     * INT experience
+     * INT weaponLevel
+     */
+    public void SaveState()
+    {
+        string s = "";
+        s += "0" + "|";
+        s += pesos.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+        PlayerPrefs.SetString("SaveState", s);
+    }
+
+    public void LoadState()
+    {
+        if (!PlayerPrefs.HasKey("SaveState"))
+        {
+            return;
+        }
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+        // Change player skin
+        pesos = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        // Change the weapon level
+    }
+}
